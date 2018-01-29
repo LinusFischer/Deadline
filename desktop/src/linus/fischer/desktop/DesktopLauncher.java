7259e6ff7db2927112647027e06f93d6949c9c29
Linus Fischer <linus.fischer@gmail.com>
2018-01-29 18:08:01 +0100

    Initial Commit

---
+++ desktop/src/linus/fischer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package linus.fischer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import linus.fischer.DeadlineGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DeadlineGame(), config);
+	}
+}
